43810bcfce6e9c713eb9620efe67d20798d11a48
Atilla MUMCULAR <atillamumcular@hotmail.com>
2022-06-28 10:07:47 +0300

    Add project files.

---
+++ src/WebApi/ProductApp.Api/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public async Task<IActionResult> Get()
+        {
+           var allList = await productRepository.GetAllAsync();
+        }
+    }
+}
